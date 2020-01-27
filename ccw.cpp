c971640df743993d5f6d724f6ac40b2c9fcd7c3d
hyoputer <ajtwlsalsdl0@naver.com>
2020-01-27 15:03:10 +0900

    cross ver2

--- ccw.cpp
+++ ccw.cpp
@@ -10,31 +10,37 @@ ll ccw(pl p1, pl p2, pl p3) {
     }
 }
 
-if(ccw(points[0], points[1], points[2]) == 0)
+int cross(pl p1, pl p2, pl p3, pl p4)
+{
+  pl points[4] = {p1, p2, p3, p4};
+  
+  if(ccw(points[0], points[1], points[2]) == 0)
   {
     if(points[2].first >= min(points[0].first, points[1].first) && points[2].first <= max(points[0].first, points[1].first))
-      printf("1\n");
+      return 1;
     else
     {
       if(ccw(points[0], points[1], points[3]) == 0 && 
          points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first))
-        printf("1\n");
+        return 1;
       else
-        printf("0\n");
+        return 0;
     }
   }
   else if (ccw(points[0], points[1], points[3]) == 0)
   {
     if(points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first))
-      printf("1\n");
+      return 1;
     else
-      printf("0\n");
+      return 0;
   }
   else
   {
     if(ccw(points[0], points[1], points[2]) != ccw(points[0], points[1], points[3]) && 
        ccw(points[2], points[3], points[0]) != ccw(points[2], points[3], points[1]))
-      printf("1\n");
+      return 1;
     else
-      printf("0\n");
+      return 0;
   }
+  
+}
