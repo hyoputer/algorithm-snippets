9832ac7a185c78ac3dd7d7b9c03e5d66f6576ccd
hyoputer <ajtwlsalsdl0@naver.com>
2020-01-27 16:13:12 +0900

    crossver3

--- ccw.cpp
+++ ccw.cpp
@@ -13,6 +13,7 @@ ll ccw(pl p1, pl p2, pl p3) {
 int cross(pl p1, pl p2, pl p3, pl p4)
 {
   pl points[4] = {p1, p2, p3, p4};
+  //printf("%d %d %d %d\n", p3.first, p3.second, p4.first, p4.second);
   
   if(ccw(points[0], points[1], points[2]) == 0)
   {
@@ -21,7 +22,8 @@ int cross(pl p1, pl p2, pl p3, pl p4)
     else
     {
       if(ccw(points[0], points[1], points[3]) == 0 && 
-         points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first))
+         ((points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first)) ||
+          (points[0].first >= min(points[2].first, points[3].first) && points[0].first <= max(points[2].first, points[3].first))))
         return 1;
       else
         return 0;
