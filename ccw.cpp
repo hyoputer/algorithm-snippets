710aeff037a1bdb055e11ebc39bac6f7819fa978
hyoputer <ajtwlsalsdl0@naver.com>
2020-01-27 14:40:38 +0900

    ccw-rev

--- ccw.cpp
+++ ccw.cpp
@@ -1,6 +1,6 @@
-int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
-    int temp = x1*y2+x2*y3+x3*y1;
-    temp = temp - y1*x2-y2*x3-y3*x1;
+ll ccw(pl p1, pl p2, pl p3) {
+    ll temp = p1.first*p2.second+p2.first*p3.second+p3.first*p1.second;
+    temp = temp - p1.second*p2.first-p2.second*p3.first-p3.second*p1.first;
     if (temp > 0) {
         return 1;
     } else if (temp < 0) {
