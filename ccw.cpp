f30c7029b16dd2adf5ff311004f4ec4bd8ffa4d2
hyoputer <ajtwlsalsdl0@naver.com>
2020-01-27 16:37:13 +0900

    ccw final

--- ccw.cpp
+++ ccw.cpp
@@ -10,39 +10,13 @@ ll ccw(pl p1, pl p2, pl p3) {
     }
 }
 
-int cross(pl p1, pl p2, pl p3, pl p4)
-{
-  pl points[4] = {p1, p2, p3, p4};
-  //printf("%d %d %d %d\n", p3.first, p3.second, p4.first, p4.second);
-  
-  if(ccw(points[0], points[1], points[2]) == 0)
-  {
-    if(points[2].first >= min(points[0].first, points[1].first) && points[2].first <= max(points[0].first, points[1].first))
-      return 1;
-    else
-    {
-      if(ccw(points[0], points[1], points[3]) == 0 && 
-         ((points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first)) ||
-          (points[0].first >= min(points[2].first, points[3].first) && points[0].first <= max(points[2].first, points[3].first))))
-        return 1;
-      else
-        return 0;
+int iscross(pi a, pi b, pi c, pi d) {
+    int r1 = ccw(a,b,c)*ccw(a,b,d);
+    int r2 = ccw(c,d,a)*ccw(c,d,b);
+    if (!r1 && !r2) {
+        if (a > b) swap(a, b);
+        if (c > d) swap(c, d);
+        return c<=b && a<=d;
     }
-  }
-  else if (ccw(points[0], points[1], points[3]) == 0)
-  {
-    if(points[3].first >= min(points[0].first, points[1].first) && points[3].first <= max(points[0].first, points[1].first))
-      return 1;
-    else
-      return 0;
-  }
-  else
-  {
-    if(ccw(points[0], points[1], points[2]) != ccw(points[0], points[1], points[3]) && 
-       ccw(points[2], points[3], points[0]) != ccw(points[2], points[3], points[1]))
-      return 1;
-    else
-      return 0;
-  }
-  
+    return r1<=0 && r2<=0;
 }
