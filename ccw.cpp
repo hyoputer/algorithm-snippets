dc72e5b80a82a1f2ae04b7fd9c3fdbee74c6dfbf
hyoputer <ajtwlsalsdl0@naver.com>
2020-01-27 14:39:08 +0900

    ccw

---
+++ ccw.cpp
@@ -0,0 +1,11 @@
+int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
+    int temp = x1*y2+x2*y3+x3*y1;
+    temp = temp - y1*x2-y2*x3-y3*x1;
+    if (temp > 0) {
+        return 1;
+    } else if (temp < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
