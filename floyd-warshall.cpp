610a00dbcfa911dbc62e0062d7e8365dbd5d72d8
hyoputer <ajtwlsalsdl0@naver.com>
2020-02-01 14:46:24 +0900

    floyd-warshall ver2

--- floyd-warshall.cpp
+++ floyd-warshall.cpp
@@ -1,80 +1,43 @@
-#include <iostream>
-#include <stdlib.h>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <vector>
-#include <utility>
-#include <string.h>
-#include <string>
-#include <cmath>
-#include <cstdio>
-#include <functional>
-#include <map>
-#include <typeinfo>
-#define INT_MAX 2147483647
-#define INT_MIN -2147483647
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pi;
+int dist[401][401];
+int pre[401][401];
 
-int dist[101][101];
-int pre[101][101];
+void floyd_warshall(int n)
+{
+  
+  for(int i = 1; i <= n; i++)
+    for(int j = 1; j <= n; j++)
+      for(int k = 1; k <= n; k++)
+      {
+              //printf("%d %d %d\n", j, k, dist[j][k]);
+
+        if(dist[j][k] > dist[j][i] + dist[i][k])
+        {
+          dist[j][k] = dist[j][i] + dist[i][k];
+          pre[j][k] = i;
+        }
+      }
+}
 
 int main()
 {
   int n, m;
   scanf("%d %d", &n, &m);
   
+  for(int i = 1; i <= n; i++)
+    for(int j = 1; j <= n; j++)
+      dist[i][j] = 1e9;
+  
   while(m--)
   {
     int a, b, d;
     scanf("%d %d %d", &a, &b, &d);
     
-    if (dist[a][b] != 0)
-      dist[a][b] = min(dist[a][b], d);
-    else
-      dist[a][b] = d;
+    dist[a][b] = min(dist[a][b], d);
     
     pre[a][b] = a;
   }
   
-  for(int i = 1; i <= n; i++)
-    for(int j = 1; j <= n; j++)
-    {
-      if (dist[j][i] != 0)
-      {
-        for(int k = 1; k <= n; k++)
-        {
-          
-          if (dist[i][k] != 0)
-          {
-            //printf("%d %d %d\n", j, k, dist[j][k]);
-            if (dist[j][k] != 0)
-            {
-              if(dist[j][k] > dist[j][i] + dist[i][k])
-              {
-                dist[j][k] = dist[j][i] + dist[i][k];
-                pre[j][k] = i;
-              }
-            }
-            else
-            {
-                dist[j][k] = dist[j][i] + dist[i][k];
-                pre[j][k] = i;
-            }
-          }
-        }
-      }
-    }
-  
-  for(int i = 1; i <= n; i++)
-  {
-    for(int j = 1; j <= n; j++)
-      printf("%d ", dist[i][j]);
-    
-    printf("\n");
-  }
+  floyd_warshall(n);
   
   return 0;
 }
