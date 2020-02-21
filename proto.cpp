82d6288927af3e22145d2111f55d794f0ca53978
hyoputer <ajtwlsalsdl0@naver.com>
2020-02-21 16:04:22 +0900

    add proto

---
+++ proto.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <string.h>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <map>
+#include <typeinfo>
+#include<bits/stdc++.h>
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<double, double> pd;
+typedef pair<ll, ll> pl;
+
+
+int main()
+{
+  FILE *inF, *outF;
+  inF = fopen("in", "r");
+  //outF = fopen("out", "w");
+  //inF = stdin;
+  outF = stdout;
+  
+  return 0;
+}
+//인풋 바꾸기!!!!
