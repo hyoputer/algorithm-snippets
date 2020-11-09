1c4d8b607fbf6821d6f6d926b7e4b0996c29c4f5
hyoputer <ajtwlsalsdl0@gmail.com>
2020-11-09 16:59:21 +0900

    proto change

--- proto.cpp
+++ proto.cpp
@@ -12,7 +12,7 @@
 #include <functional>
 #include <map>
 #include <typeinfo>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define INF 1e9
 using namespace std;
 typedef long long ll;
@@ -20,15 +20,12 @@ typedef pair<int, int> pi;
 typedef pair<double, double> pd;
 typedef pair<ll, ll> pl;
 
-
 int main()
 {
     FILE *inF, *outF;
     inF = fopen("in", "r");
     outF = fopen("out", "w");
-    //inF = stdin;
-    //outF = stdout;
-  
+
     return 0;
 }
 //인풋 바꾸기!!!!
