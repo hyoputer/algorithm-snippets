4821b8494cf810135210d8e076fc0689f7bf36a9
hyoputer <ajtwlsalsdl0@gmail.com>
2020-11-10 14:22:22 +0900

    topsort typo

--- topology-sort.cpp
+++ topology-sort.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
+#include <string.h>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <functional>
+#include <map>
+#include <typeinfo>
+#include <bits/stdc++.h>
+#define INF 1e9
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<double, double> pd;
+typedef pair<ll, ll> pl;
+
 vector<int> nextv[501];
 int pre[501];
 vector<int> sorted;
@@ -36,10 +58,8 @@ void topsort()
 int main()
 {
     FILE *inF, *outF;
-    inF = fopen("in", "r");
-    outF = fopen("out", "w");
-    //inF = stdin;
-    //outF = stdout;
+    inF = stdin;
+    outF = stdout;
 
     fscanf(inF, "%d", &n);
 
@@ -66,3 +86,27 @@ int main()
             fscanf(inF, "%d", &num);
         }
     }
+
+    topsort();
+
+    int dp[501] = {};
+
+    for (int i = 0; i < sorted.size(); i++)
+    {
+        int curtime = 0;
+        for (int j = 0; j < prev[sorted[i]].size(); j++)
+        {
+            curtime = max(dp[prev[sorted[i]][j]], curtime);
+        }
+
+        dp[sorted[i]] = curtime + time[sorted[i]];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        fprintf(outF, "%d\n", dp[i]);
+    }
+
+    return 0;
+}
+//인풋 바꾸기!!!!
