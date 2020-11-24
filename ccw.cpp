44401d95e1568e636a52e41ec95df6bd0e91b439
hyoputer <ajtwlsalsdl0@gmail.com>
2020-11-24 13:25:29 +0900

    ccw 출처

--- ccw.cpp
+++ ccw.cpp
@@ -1,3 +1,4 @@
+// from https://www.acmicpc.net/blog/view/27
 ll ccw(pl p1, pl p2, pl p3) {
     ll temp = p1.first*p2.second+p2.first*p3.second+p3.first*p1.second;
     temp = temp - p1.second*p2.first-p2.second*p3.first-p3.second*p1.first;
