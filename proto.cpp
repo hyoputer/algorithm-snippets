3f2465907a5510f5453bc3bf40a1e2aec3c678ba
hyoputer <ajtwlsalsdl0@gmail.com>
2022-07-07 09:48:08 +0900

    add PDBS

--- proto.cpp
+++ proto.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pi;
 typedef pair<double, double> pd;
 typedef pair<ll, ll> pl;
+typedef tree<
+    pl,
+    null_type,
+    less<pl>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+    ordered_set;
 
 int main()
 {
@@ -13,6 +22,7 @@ int main()
 
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
+
     return 0;
 }
 //인풋 바꾸기!!!!
