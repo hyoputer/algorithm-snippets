d6fa10949a20574223b16fecab448d4be14a3d69
hyoputer <ajtwlsalsdl0@gmail.com>
2022-07-25 09:24:04 +0900

    mid error comment

--- binary-search.cpp
+++ binary-search.cpp
@@ -3,7 +3,7 @@ int b = v.size();
 
 while(f < b)
 {
-    int m = (b - f) / 2 + f;
+    int m = (b - f) / 2 + f; //mid 오버플로우 방지 and f+b가 음수 가능성 있을 때 버그 방지
     if (v[m] < num)
     {
         f = m + 1;
