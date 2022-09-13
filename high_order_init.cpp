6b39431dcd212ed542d47c971dae8f11c2aa9990
hyoputer <ajtwlsalsdl0@gmail.com>
2022-09-13 15:35:29 +0900

    Create high_order_init.cpp

---
+++ high_order_init.cpp
@@ -0,0 +1,4 @@
+int m, n, o, p, q, r, s, t, u, v, w;
+	cin >> m >> n >> o >> p >> q >> r >> s >> t >> u >> v >> w;
+
+	int(*arr)[v][u][t][s][r][q][p][o][n][m] = (int (*)[v][u][t][s][r][q][p][o][n][m])malloc(w * sizeof *arr);
